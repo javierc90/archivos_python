203b3ba114280f40cd1a5b8493f63476966b2dd8
Javier Carugno <javiercarugno@gmail.com>
2023-08-24 21:02:31 -0300

    Add files via upload
    Resuelvo el ejercicio

--- ejemplo_3.py
+++ ejemplo_3.py
@@ -27,7 +27,27 @@ def altura_promedio(genero):
 
     # --- Comience aquí a desarrollar su código ---
 
+    with open('alturas.csv') as csvfile:
+        # Leer todos los datos y almacenarlos en una 
+        # lista de diccionarios
+        personas = list(csv.DictReader(csvfile))
+
+    altura_total = 0
+    cantidad_genero = 0
+
+    for i in range(len(personas)):
+        if personas[i]['genero'] == genero:
+            cantidad_genero += 1
+            altura_total += float(personas[i]['altura'])
+    
+    promedio_altura = 0
+
+    if cantidad_genero > 0:
+        promedio_altura = altura_total / cantidad_genero
+
+    print(f"Altura promedio del genero {genero}: {promedio_altura}")
+
 
 if __name__ == '__main__':
     print("Bienvenidos a otra clase de Inove con Python")
-    altura_promedio("femenino")
+    altura_promedio("masculino")
